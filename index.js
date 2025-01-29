a01443dc0e8380b0e76a77bf45db87eff8db8306
dip1995 <vyas.diptesh@gmail.com>
2025-01-29 21:49:13 +0530

    dynamic-data

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const express = require('express');
+const connectDB = require('./src/config 9.22.56 PM/config/database');
 const app = express();
-app.listen(3000,() => {
- console.log('Server start at port 3000')   
+
+const User = require('./src/config 9.22.56 PM/models/user');
+
+app.use(express.json());
+
+app.post('/user', async (req,res) => {
+  console.log(req.body)
+  const user = new User(req.body);
+  try{
+    await user.save();
+    res.send('Record Inserted!!')
+  }catch(err){
+    res.send('Error in Insert Record!!')
+  }
+    
 })
+
+connectDB().then(() => {
+    console.log('Database Connected!') 
+    app.listen(3000,() => {
+    console.log('Server start at port 3000')   
+    })
+}).catch(() => {
+    console.log('Database not Connected!') 
+})
+
+
+
